d7829ebd0860c9d777ccde1106d6100131589969
Andre <andrehmf23@gmail.com>
2026-04-19 17:15:35 -0300

    Otimizações e reestruturação da classe

--- main.py
+++ main.py
@@ -1,64 +1,13 @@
-# -----------------------------------------
-#                [Teste]
-# -----------------------------------------
-
-import os
-import shutil
-
-def recreate_folder(path):
-    """
-    Deleta a pasta especificada (se existir) e a recria vazia.
-    """
-    try:
-        # Remove a pasta se existir
-        if os.path.exists(path):
-            shutil.rmtree(path)  # Remove pasta e todo o conteúdo
-            print(f"Pasta '{path}' removida com sucesso.")
-
-        # Cria a pasta novamente
-        os.makedirs(path, exist_ok=True)
-        print(f"Pasta '{path}' criada com sucesso.")
-
-    except PermissionError:
-        print(f"Erro: Permissão negada para modificar '{path}'.")
-    except OSError as e:
-        print(f"Erro ao manipular a pasta '{path}': {e}")
-
-# -----------------------------------------
-
-
 from utils.fileOrganizer import *
-
-recreate_folder("./test")
-
-start = time.perf_counter()
 
 org = FileOrganizer(
     target=r"/home/andre/Músicas",
     destination=r"./test",
     level_limit=10,
     copy=True,
-    organizer=Organizer.ALPHABETICAL,
-    date_tree=True,
-    data_order="dmy"
+    structure="md",
 )
 
-print("A: " + org.detect_script(c))
-print("a: " + org.detect_script("arrow"))
-print("ç: " + org.detect_script("caça"))
-print("部: " + org.detect_script("部"))
-
-
-"""
-
-end = time.perf_counter()
-print(f"Tempo de criação: {end - start:.6f} segundos")
-
-start = time.perf_counter()
+org._destination_resolve
 
 org.collect()
-
-end = time.perf_counter()
-print(f"Tempo de execução: {end - start:.6f} segundos")
-
-"""
--- utils/fileOrganizer.py
+++ utils/fileOrganizer.py
@@ -1,9 +1,7 @@
 from pathlib import Path
 import shutil
-from enum import Enum, auto
 from datetime import datetime
-import unicodedata
-import time
+import os
 
 # ----------------------------
 # Presets e formatos de data
@@ -14,16 +12,6 @@ DATE_FORMAT = {
     'm': '%m',
     'd': '%d'
 }
-
-# ----------------------------
-# Enum de organização
-# ----------------------------
-
-class Organizer(Enum):
-    DIRECT = auto()
-    ALPHABETICAL = auto()
-    DATE = auto()
-    SUFFIX = auto()
 
 
 # ----------------------------
@@ -36,17 +24,14 @@ class FileOrganizer:
     Identifica os arquivos no caminho alvo e depois de passar 
     por filtros move-os ou copia-os para o caminho destinado
     """
-    
+
     def __init__(
         self,
         target: str, # Caminho da coleta
         destination: str, # Caminho da organização
         level_limit: int | None = None, # Limite de leitura de profundidade
-        copy: bool = True,
-        organizer: Organizer = Organizer.DIRECT,
-        language: str = None,
-        date_tree: bool = False,
-        data_order: str = "ymd"
+        copy: bool = True, # Copiar arquivos, caso contrario os move
+        structure: str = None, # Opcional
     ):
         self.target = Path(target)
         self.destination = Path(destination)
@@ -54,15 +39,9 @@ class FileOrganizer:
         self.level_limit = level_limit
         self.copy = copy
 
-        self.language = language
-
-        self.organizer = organizer
-        self.date_tree = date_tree
-
-        # Preset de data
-        self.date_order = data_order.lower()
+        self.structure = structure.lower() if structure else None
         
-        # Filtros
+        # Filtros de busca
         self.filter = {
             "name": None,
             "type": None,
@@ -70,6 +49,9 @@ class FileOrganizer:
         }
 
         self.destination.mkdir(parents=True, exist_ok=True)
+
+        # Variáveis temporárias
+        self._destination_resolve = self.destination.resolve()
         
     # ----------------------------
     # Filtros
@@ -87,25 +69,6 @@ class FileOrganizer:
         else:
             self.filter["date"] = None
     
-    def detect_script(c):
-        return unicodedata.name(c, '')
-
-    # ----------------------------
-    # Caminho único
-    # ----------------------------
-    # Reescreve o caminho para ser único se necessário
-
-    def single_files(self, destination: Path) -> Path:
-        if not destination.exists():
-            return destination
-
-        i = 1
-        while True:
-            new_destination = destination.with_stem(f"{destination.stem}_{i}")
-            if not new_destination.exists():
-                return new_destination
-            i += 1
-    
     # ----------------------------
     # Coleta recursiva
     # ----------------------------
@@ -114,86 +77,116 @@ class FileOrganizer:
     # Se arquivo, filtra e envia para organize
 
     def collect(self):
-        self.__collect(self.target)
 
-    def __collect(self, current: Path, level: int = 0):
+        # Otimização do stat do Path
+        needs_stat = (
+            self.filter["date"] is not None or
+            (self.structure and any(c in self.structure for c in "ymdt"))
+        )
+
+        self._collect(self.target, needs_stat)
+
+    def _collect(self, current: Path, needs_stat: bool, level: int = 0):
 
         if self.level_limit is not None and level > self.level_limit:
             return
-        
+
         for item in current.iterdir():
             if item.is_file():
-
-                # Verificação do filtro
-                if self.filter["name"] is not None and item.name != self.filter["name"]:
+                
+                if self.filter["name"] is not None and self.filter["name"] not in item.name:
                     continue
 
-                if self.filter["type"] is not None and item.suffix.lower().lstrip('.') != self.filter["type"]:
+                if self.filter["type"] is not None and self.filter["type"] != item.suffix.lower().lstrip('.'):
                     continue
 
-                stat = item.stat()
+                # Otimização do stat
+                stat = item.stat() if needs_stat else None
                 
                 if self.filter["date"] is not None:
                     start, end = self.filter["date"]
                     if not (start <= stat.st_mtime <= end):
                         continue
 
-                start = time.perf_counter()
-
-                self.organize(item, stat)
-
-                end = time.perf_counter()
-                sub = end - start
-                if sub > 0.1: print(f"Tempo de organize: {sub:.6f} segundos [{item}]")
+                self._organize(item, self.destination, stat)
 
             elif item.is_dir():
-                # Impede o acesso do caminho de organização
-                if item.resolve() == self.destination.resolve():
+
+                if item.resolve() == self._destination_resolve:
                     continue
-                self.__collect(item, level + 1)
+
+                self._collect(item, needs_stat, level + 1)
+    
+    # ----------------------------
+    # Caminho único
+    # ----------------------------
+    # Reescreve o caminho para ser único se necessário
+
+    def _single_files(self, destination: Path) -> Path:
+
+        # Assume que já está criado
+
+        i = 1
+        while True:
+            new_destination = destination.with_stem(f"{destination.stem}_{i}")
+            if not new_destination.exists():
+                return new_destination
+            i += 1
 
     # ----------------------------
     # Organização
     # ----------------------------
 
-    def organize(self, item: Path, stat):
-        destination = self.destination
+    def _structured_path(self, target: Path, destination: Path, stat: os.stat_result):
 
-        # Alfabético
-        if self.organizer == Organizer.ALPHABETICAL:
-            c = next((ch for ch in item.name if ch.isalpha()), '#').upper()
+        """
+        Estrutura o caminho do destino deacordo com o comandos do structure
+        """
 
-            if self.language is None:
+        date = None
+
+        for command in self.structure:
+
+            # Alfabético
+            if command == 'a':
+                c = next((ch for ch in target.name if ch.isalpha()), '#').upper()
                 destination /= c
-            elif self.language == self.detect_script(c):
-                destination /= c
-            else:
-                destination /= 'others'
 
-        # Data
-        elif self.organizer == Organizer.DATE:
-            date = datetime.fromtimestamp(stat.st_mtime)
-            formats = [DATE_FORMAT[p] for p in self.date_order if p in DATE_FORMAT]
+            # Dia, Mês ou ano
+            elif command in ('y','m','d'):
+                if date is None:
+                    date = datetime.fromtimestamp(stat.st_mtime)
+                destination /= date.strftime(DATE_FORMAT[command])
+                
+            # Data (Time)
+            elif command == 't':
+                if date is None:
+                    date = datetime.fromtimestamp(stat.st_mtime)
+                destination /= date.strftime("%Y-%m-%d")
 
-            if self.date_tree:
-                for format in formats:
-                    destination /= date.strftime(format)
-            else:
-                destination /= date.strftime("-".join(formats))
+            # Sufixo
+            elif command == 's':
+                suffix = target.suffix.lower().lstrip('.') or 'WITHOUT_SUFFIX'
+                destination /= suffix
 
-        # Sufixo
-        elif self.organizer == Organizer.SUFFIX:
-            suffix = item.suffix.lstrip('.').lower() or 'SEM_EXTENSAO'
-            destination /= suffix
+            # LANGUAGE -> Under contruction
+
+        return destination
+
+    def _organize(self, target: Path, destination: Path, stat: os.stat_result):
+
+        # Estrutura caminho se uma estrutura existir
+        if self.structure:
+            destination = self._structured_path(target, destination, stat)
 
         # Cria diretórios
         destination.mkdir(parents=True, exist_ok=True)
 
         # Evita sobrescrita
-        destination = self.single_files(destination / item.name)
+        destination = self._single_files(destination / target.name)
 
         # Copiar ou mover
         if self.copy:
-            shutil.copy2(item, destination)
+            shutil.copy2(target, destination)
         else:
-            shutil.move(item, destination)
+            shutil.move(target, destination)
